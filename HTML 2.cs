a6ce6350ebeacfb06e1d0467a6c0c742e87f069d
Oleksander08 <look98727@gmail.com>
2025-06-14 23:01:06 +0300

    Add files via upload

---
+++ HTML 2.cs
@@ -0,0 +1,126 @@
+﻿< !DOCTYPE html >
+< html lang = "en" >
+< head >
+    < meta charset = "UTF-8" >
+    < meta name = "viewport" content = "width=device-width, initial-scale=1.0" >
+    < title > FenixCoin </ title >
+    < link href = "https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;600;700&display=swap" rel = "stylesheet" >
+    < link rel = "stylesheet" href = "Home.css" >
+</ head >
+< body >
+    < header >
+    < img class= "logo" src = "https://png.pngtree.com/png-vector/20220630/ourmid/pngtree-phoenix-logo-template-antique-bird-png-image_5584157.png" alt = "" >
+    < div class= "interface" >
+        < a href = "" > Home </ a >
+        < a href = "" > Market </ a >
+        < a href = "" > Rating </ a >
+    </ div >
+    < div class= "user" >
+        < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Avatar.png" alt = "" >
+        < div class= "profile-info" >
+            < span class= "usname" > Tony Stark </ span >
+            < div class= "money" >
+                < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Coin.png" alt = "Coin" >
+                < span class= "money1" > 19 247 </ span >
+            </ div >
+        </ div >
+    </ div >
+    </ header >
+        < main >
+            < div class= "form" >
+                < div class= "item" >
+                    < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+                    < div class= "shop" >
+                        < p class= "name" > Click Accelerator </ p >
+                        < p class= "description" > Speed of earning x10</p>
+                        <span class= "price" >< img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Coin.png" > 40 000 </ span >
+                    </ div >
+                    < button class= "buy" > Buy </ button >
+                </ div >
+                < div class= "item" >
+                    < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+                    < div class= "shop" >
+                        < p class= "name" > Coin Multiplier </ p >
+                        < p class= "description" > ClickCoins per click x10</p>
+                        <span class= "price" >< img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Coin.png" > 40 000 </ span >
+                    </ div >
+                    < button class= "buy" > Buy </ button >
+                    </ div >
+                    < div class= "item" >
+                        < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+                        < div class= "shop" >
+                            < p class= "name" > Power Tap </ p >
+                            < p class= "description" > ClickCoins per click x2</p>
+                            <span class= "price" >< img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Coin.png" > 10 000 </ span >
+                        </ div >
+                        < button class= "buy" > Buy </ button >
+                        </ div >
+                        < div class= "item" >
+                            < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+                            < div class= "shop" >
+                                < p class= "name" > Golden Touch </ p >
+                                < p class= "description" > random bonus on click</p>
+                                <span class= "price" >< img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Coin.png" > 40 000 </ span >
+                            </ div >
+                            < button class= "buy" > Buy </ button >
+                            </ div >
+                            < div class= "item" >
+                                < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+                                < div class= "shop" >
+                                    < p class= "name" > Coin Stream </ p >
+                                    < p class= "description" > passive income x10</p>
+                                    <span class= "price" >< img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Coin.png" > 40 000 </ span >
+                                </ div >
+                                < button class= "bbuy" > Buy </ button >
+                                </ div >
+                                < div class= "item" >
+                                    < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+                                    < div class= "shop" >
+                                        < p class= "name" > Mining Drone </ p >
+                                        < p class= "description" > automated clicks for 1 min</p>
+                                        <span class= "price" >< img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Coin.png" > 100 000 </ span >
+                                    </ div >
+                                    < button class= "buy" > Buy </ button >
+                                </ div >
+        </ div >
+
+
+        < div class= "con" >
+            < div class= "li" ></ div >
+            < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+            < div class= "bl" >
+                < p > 19 247 </ p >
+                < p > Total ClickCoins </ p >
+            </ div >
+            < div class= "li" ></ div >
+            < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+            < div class= "bl" >
+                < p > 20 </ p >
+                < p > Coins per Click</p>
+            </div>
+            <div class= "li" ></ div >
+            < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Square.png" alt = "" >
+            < div class= "bl" >
+                < p > 1 / sec </ p >
+                < p > Passive Income </ p >
+            </ div >
+        </ div >
+        < div class= "coin" >
+            < img src = "C:\Users\andri\OneDrive\Робочий стіл\1\img\Placeholder Circle.png" alt = "Coin" >
+        </ div >
+    </ main >
+< footer >
+    < ul >
+        < li >
+        < a href = "" > About game </ a >
+        </ li >
+        < li >
+            < a href = "" > Help center </ a >
+        </ li >
+        < li >
+            < a href = "" > Rules </ a >
+        </ li >
+    </ ul >
+</ footer >
+</ body > More actions
+</ html >
---
+++ HTML.cs
@@ -0,0 +1,237 @@
+﻿{
+    More actions
+    margin: 0;
+padding: 0;
+outline: none;
+    font - family: 'Work Sans', serif;
+}
+
+footer a
+{
+    color: #ffffff;
+    font-size: 16px;
+    text-decoration: none;
+}
+
+footer {
+    background-color: #61646B;
+    padding: 24px;
+}
+
+footer ul
+{
+    display: flex;
+    justify-content: center;
+    gap: 96px;
+    list-style-type: none;
+}
+
+.logo{
+    display: flex;
+width: 93.75px;
+}
+
+header {
+    display: flex;
+justify - content: space - between;
+align - items: center;
+background: #E0E0E0;
+    padding - top: 16px;
+padding - right: 120px;
+padding - bottom: 16px;
+padding - left: 120px;
+width: auto;
+height: 82px;
+}
+
+.interface a
+{
+    justify-content: center;
+    margin: 0 15px;
+    text-decoration: none;
+    color: #61646B;
+}
+
+.interface{
+    display: flex;
+    gap: 126px;
+}
+
+.user {
+    display: flex;
+align - items: center;
+padding: 5px 10px;
+border - radius: 8px;
+background: #e0e0e0; 
+    font - family: Arial, sans-serif;
+}
+
+.user img
+{
+    width: 48px;
+    height: 48px;
+    margin-right: 10px;
+}
+
+.user {
+    display: flex;
+flex - direction: column;
+color: #555;
+}
+
+.usname {
+    font-size: 16px;
+color: #61646B;
+}
+
+.money {
+    margin-top : 6px;
+display: flex;
+align - items: center;
+font - size: 16px;
+}
+
+.money img
+{
+    width: 16px;
+    height: 16px;
+    margin-right: 8px;
+}
+
+.money1 {
+   font-weight: 500;
+}
+
+.item {
+  display: flex;
+padding: 16px 24px;
+border: 1px solid #AFB1B6;
+  max-width: 384px;
+border - radius: 15px;
+align - items: start;
+justify - content: space - between;
+margin - left: 120px;
+margin - top: 12px;
+
+}
+
+.item img
+{
+    width: 48px;
+}
+
+.shop {
+    display: flex;
+align - items: flex - start;
+gap: 2px;
+flex - direction: column;
+margin - right: 114px;
+}
+
+.shop img
+{
+    width: 24px;
+}
+
+.name {
+    color: #121212;
+    font - size: 16px;
+font - weight: 500;
+}
+
+.description {
+    color: #61646b;
+    font - size: 16px;
+font - weight: 400;
+margin: 2px auto;
+}
+
+.price {
+    color: #121212;
+    font - size: 16px;
+display: flex;
+align - items: center;
+gap: 8px;
+}
+  
+.buy {
+    border: none;
+color: #ffffff;
+    font - size: 14px;
+align - self: flex - end;
+padding: 10px 16px;
+border - radius: 12px;
+cursor: pointer;
+background - color: #000000;
+    
+}
+
+.item button
+{
+    border: none;
+    color: #121212;
+    font-size: 14px;
+    align-self: flex-end;
+    padding: 10px 16px;
+    border-radius: 12px;
+    cursor: pointer;
+    background-color: #efefef;
+    transition: 0.3s;
+}
+
+.item button:hover {
+    color: #efefef;
+    background - color: #121212;
+}
+
+main {
+    display: flex;
+position: relative;
+align - items: flex - start;
+gap: 24px;
+}
+
+.form {
+    margin-bottom: 60px;
+margin - top: 40px;
+}
+
+.con {
+    border-radius: 15px;
+height: 106px;
+margin - top: 55px;
+background - color: #43464D;
+    display: flex;
+gap: 24px; 
+}
+
+.li {
+    width: 1px;
+background - color: #E0E0E0;
+}
+
+.bl {
+    color: white;
+padding: 20px;
+text - align: left;
+width: 150px;
+display: flex;
+flex - direction: column;
+justify - content: center;
+align - items: flex - start; 
+}
+
+.coin {
+    display: block;
+margin: 0;
+position: absolute;
+top: 240px;
+right: 240px;
+margin - right: 316px;
+}
+
+.coin img
+{
+    display: block;
+    width: 400px;
+}
